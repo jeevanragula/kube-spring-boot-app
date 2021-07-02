393c47650ecc38084d6e417d11036907d7a8fa5d
jeevanragula <jeevanragula@gmail.com>
2021-07-02 20:40:49 +0530

    source

---
+++ src/main/java/com/roughnotes/kubespringbootapp/KubeSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.roughnotes.kubespringbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubeSpringBootAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KubeSpringBootAppApplication.class, args);
+    }
+
+}
